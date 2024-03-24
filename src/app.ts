3b14b1846b46406f48f8f8319cd848dbdc912905
Quzeem Agbaje <quzeemagbaje@gmail.com>
2024-03-24 14:08:53 +0100

    Add security packages

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import httpError from 'http-errors';
+import cors from 'cors';
+import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
+import hpp from 'hpp';
+import compression from 'compression';
 
 import { globalErrorHandler } from './middlewares/error.middleware';
 import { authRouter } from './routes/auth.routes';
@@ -8,8 +14,34 @@ import { productRouter } from './routes/product.routes';
 
 const app = express();
 
+// Enable cors
+app.use(cors());
+
+// Parse incoming request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set security HTTP response headers
+app.use(helmet());
+
+// Limit repeated requests to API endpoints, preventing abuse and potential denial-of-service attacks
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  limit: 100,
+  message: 'Too many requests, please try again later.',
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+});
+app.use(limiter);
+
+// Sanitize user-supplied data to prevent MongoDB query injection
+app.use(mongoSanitize());
+
+// sanitize and protect against parameter pollution in Express.js applications
+app.use(hpp());
+
+// Reduces the size of HTTP responses by compressing them before sending them over the network
+app.use(compression());
 
 app.get('/', (req: Request, res: Response) => res.status(200).send('Server is up and running!!!'));
 app.use('/api/v1/auth', authRouter);
